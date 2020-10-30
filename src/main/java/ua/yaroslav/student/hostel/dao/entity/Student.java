059610368ccf5635fcc8b4306ea132447939808e
yaroslav-yukhymiv <sams8ung888@gmail.com>
2020-10-30 23:56:44 +0200

    Add first file

---
+++ src/main/java/ua/yaroslav/student/hostel/dao/entity/Student.java
@@ -0,0 +1,64 @@
+package ua.yaroslav.student.hostel.dao.entity;
+
+import java.time.LocalDateTime;
+
+public class Student {
+    private int id;
+    private String firstName;
+    private String secondName;
+    private int roomNumber;
+    private String faculty;
+    private String birthday;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) { this.birthday = birthday; }
+
+    public LocalDateTime getLocalDateTime(){
+        LocalDateTime localDateTime = LocalDateTime.parse(birthday);
+        return localDateTime;
+    }
+
+}
---
+++ src/test/java/ua/yaroslav/student/hostel/JavaAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.yaroslav.student.hostel;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JavaAdvanceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
